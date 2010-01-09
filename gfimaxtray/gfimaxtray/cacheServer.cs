b8a05ad9e161c416753a29daaa8dd8196d01b725
stroendle <stroendle@b83b72de-fd5a-11de-8589-c9554b7fe0d5>
2010-01-09 22:51:32 +0000

    First version, not yet fully functional

---
+++ gfimaxtray/gfimaxtray/cacheServer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gfimaxtray
+{
+    class cacheServer
+    {
+        public string serverid = "";
+        public string servername = "";
+        public cacheSite serversite;
+
+        public cacheServer(string tmpservername, string tmpserverid, cacheSite tmpsite)
+        {
+            serverid = tmpserverid;
+            servername = tmpservername;
+            serversite = tmpsite;
+        }
+    }
+}
---
+++ gfimaxtray/gfimaxtray/frmCheckresults.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace gfimaxtray
+{
+    public partial class frmCheckresults : Form
+    {
+        public frmCheckresults()
+        {
+            InitializeComponent();
+        }
+    }
+}
